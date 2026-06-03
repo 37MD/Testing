30af3131c9db827f758afef4405151ff6b8739d9
37MD <150575622+37MD@users.noreply.github.com>
2026-06-03 07:33:52 +0530

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,23 @@
+{
+  "name": "Quick Commerce Finder",
+  "short_name": "QCFinder",
+  "description": "Compare Quick Commerce Prices in Thane",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#f4f4f4",
+  "theme_color": "#cccccc",
+  "icons": [
+    {
+      "src": "icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png",
+      "purpose": "any maskable"
+    },
+    {
+      "src": "icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png",
+      "purpose": "any maskable"
+    }
+  ]
+}
