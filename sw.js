e44037564500d01f16fff2fad6b760f7dda1656c
37MD <150575622+37MD@users.noreply.github.com>
2026-06-03 09:26:11 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,43 @@
-const CACHE_NAME = 'qc-finder-cache-v1';
-const urlsToCache = [
-  './index.html',
-  './manifest.json'
+var GHPATH = '/QuickCommerce';
+var APP_PREFIX = 'qcfinder_';
+var VERSION = 'version_01';
+
+var URLS = [
+  `${GHPATH}/`,
+  `${GHPATH}/index.html`
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+var CACHE_NAME = APP_PREFIX + VERSION;
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(request) {
+      if (request) return request;
+      else return fetch(e.request);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(URLS);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      var cacheWhitelist = keyList.filter(function(key) {
+        return key.indexOf(APP_PREFIX) === 0;
+      });
+      cacheWhitelist.push(CACHE_NAME);
+      return Promise.all(keyList.map(function(key, i) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          return caches.delete(keyList[i]);
         }
-        return fetch(event.request);
-      })
+      }));
+    })
   );
 });
